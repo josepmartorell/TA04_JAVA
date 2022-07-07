a3047b2195748a80d7376b4ef891c43cc53005fd
josepmartorell <martorelljosep@gmail.com>
2022-07-07 13:44:23 +0200

    prueba app 2

---
+++ src/PruebaApp2.java
@@ -0,0 +1,22 @@
+
+public class PruebaApp2 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int N = 5;
+		double A = 4.56;
+		char C = 'a';
+		int a=  C;
+		
+		System.out.println("valor de la variable N = " +N+ "\nValor de A = " +A+ "\nValor de C = " +C);
+		System.out.println("N + A = " + (N + A));
+		System.out.println("N - A = " + (N - A));
+		System.out.println("Valor numérico de C = " + a);
+		
+	
+
+	}
+
+}
