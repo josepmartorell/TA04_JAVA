d848c52a530c71236ffa48f4167c9ebd83d27a76
josepmartorell <martorelljosep@gmail.com>
2022-07-07 12:53:39 +0200

    ta04 ejercicio1

---
+++ src/PruebaApp1.java
@@ -0,0 +1,26 @@
+
+public class PruebaApp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int suma;
+		float var1 = 7;
+		float var2 = 3;
+		
+		double resultOperation;
+		
+		resultOperation = var1 + var2;
+		System.out.println("suma var2 + var2 " + resultOperation);
+		resultOperation = var1 - var2;
+		System.out.println("suma var2 - var2 " + resultOperation);
+		resultOperation = var1 / var2;
+		System.out.println("suma var2 / var2 " + resultOperation);
+		resultOperation = var1 % var2;
+		System.out.println("suma var2 % var2 " + resultOperation);
+			
+
+	}
+
+}
